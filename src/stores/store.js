8eefa9a40163c8423001440122bb11b19b5e9b6f
Thura Htet Zaw <thurahtetzaw02@gmail.com>
2024-11-02 01:04:16 +0630

    copied store.js from test branch

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,79 +1,85 @@
-import {defineStore} from 'pinia';
-import {ref,reactive, computed} from 'vue';
-export const useTestStore = defineStore('test-store',() => {
+import { defineStore } from "pinia";
+import { ref, reactive, computed } from "vue";
+import { generateWords } from "@/utils/GenerateWords";
+export const useTypingStore = defineStore("typing-store", () => {
+  let testResult = reactive({});
+  const charStats = reactive({
+    accuracy: 0,
+    correct: 0,
+    incorrect: 0,
+    missing: 0,
+    extra: 0,
+  });
+  const timePassed = ref(0);
+  const status = ref("");
+  const words = reactive([]);
+  const selectedMenuOption = ref("time");
+  const symbol = ref(false);
+  const number = ref(false);
+  const time_constant = ref(30);
+  const word_constant = ref(50);
+  const difficulty = ref("normal");
 
-    
-    let testResult = reactive({});
-    const charStats = reactive({accuracy:0,correct:0,incorrect:0,missing:0,extra:0});
-    const timePassed = ref(0);
-
-
-    const getTimeArr = computed(() => {
-        return Object.keys(testResult);
-    })
-    
-    const getWpmArr = computed(() => {
-        return Object.values(testResult).map((res) => res.wpm);
-    })
-
-    const getRawArr = computed(() => {
-        return Object.values(testResult).map((res) => res.raw);
-    })
-    const getErrorArr = computed(() => {
-        return Object.values(testResult).map((res) => res.errors);
-    })
-    
-
-
-
-    function clearAll() {
-        Object.keys(testResult).forEach((key) => delete testResult[key]);
-        Object.keys(charStats).forEach((key) => delete charStats[key]);
+  const getWords = computed(() => {
+    if (selectedMenuOption.value === "word") {
+      words.length = 0;
+      words.push(
+        ...generateWords(word_constant.value, number.value, symbol.value).map(
+          (word) => word.val
+        )
+      );
+      return words;
+    } else {
+      words.length = 0;
+      words.push(
+        ...generateWords(200, number.value, symbol.value).map(
+          (word) => word.val
+        )
+      );
+      return words;
     }
-    
+  });
+  const getTimeArr = computed(() => {
+    return Object.keys(testResult);
+  });
 
-    
+  const getWpmArr = computed(() => {
+    return Object.values(testResult).map((res) => res.wpm);
+  });
 
-    return {testResult,charStats,timePassed,getTimeArr,getWpmArr,getRawArr,getErrorArr,clearAll};
+  const getRawArr = computed(() => {
+    return Object.values(testResult).map((res) => res.raw);
+  });
+  const getErrorArr = computed(() => {
+    return Object.values(testResult)
+      .map((res) => res.errors)
+      .filter((res) => res !== 0);
+  });
+
+  const restart = () => {
+    Object.keys(testResult).forEach((key) => delete testResult[key]);
+    Object.keys(charStats).forEach((key) => delete charStats[key]);
+    timePassed.value = 0;
+    status.value = "";
+  };
+
+  return {
+    testResult,
+    charStats,
+    timePassed,
+    getTimeArr,
+    getWpmArr,
+    getRawArr,
+    getErrorArr,
+    restart,
+    status,
+    symbol,
+    number,
+    time_constant,
+    word_constant,
+    difficulty,
+    selectedMenuOption,
+    getWords,
+    words,
+  };
 });
-
-
-
-
-
-
-export const useTypingStateStore = defineStore('typing-state-store',() => {
-    // TODO: merge other values here
-    const status = ref('')
-    const selectedMenuOption = ref('time');
-    const symbol = ref(false);
-    const number = ref(false);
-    const time_constant =  ref('30');
-    const word_constant =  ref('30');
-    const difficulty =  ref('normal');
-    
-
-    
-    const setSelectedMenu = (value) => {
-        console.log(value);
-        selectedMenuOption.value = value; 
-    }
-
-    const setStatus = (value) => {
-        status.value = value;
-    }
-    
-    const setTimeConstant = (value) => {
-        time_constant.value = value;
-    }
-    
-
-    return {
-        status,setSelectedMenu,symbol,number,time_constant,word_constant,difficulty,setStatus,setTimeConstant,selectedMenuOption
-    }
-
-})
-
-
-
-
